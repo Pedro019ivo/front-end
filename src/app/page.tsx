000b03f69e889de226dd12c1d129fc24b8547de7
Pedro Ivo <pedro019ivo@gmail.com>
2025-10-20 20:37:53 -0300

    instalando bibliotecas necessárias para o projeto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 //import Image from "next/image";
 
-import { Button } from "@/components/Button";
-
 export default function Home() {
   return <Button text="clica ai"></Button>;
 }
--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-interface ButtonProps {
-  //children: ReactNode
-  text: string;
-}
-export function Button({ text }: ButtonProps) {
-  return <button>{text}</button>;
-}
